c0f310250991e9a317f4aaa762340d195c5a0ff4
William Fligor <williamtfligor@gmail.com>
2025-03-10 20:14:38 -0700

    feat: Add fzzy function for real fzf fuzzy searching within files

--- .chezmoitemplates/functions.sh
+++ .chezmoitemplates/functions.sh
@@ -7,6 +7,20 @@ fif() {
     rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
 }
 
+# fzf fuzzy funding within files
+fzzy() {
+    fzf --multi --ansi --query "${*:-}" \
+        --bind "start:reload:rg --line-number --no-heading --color always {q}" \
+        --color "hl:-1:underline,hl+:-1:underline:reverse" \
+        --prompt 'notes-selector:fzf> ' \
+        --delimiter : \
+        --preview 'bat --color=always {1} --highlight-line {2}' \
+        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+        --bind 'enter:become:sort -u {+f1}' \
+        --bind 'tab:toggle+change-preview-window(~0:+1)+change-preview:echo "SELECTED FILES:"; sort -u {+f1} | sed "s/^/  - /"' \
+        --bind 'focus:+change-preview-window(+{2}+3/3)+change-preview:bat --color=always {1} --highlight-line {2}'
+}
+
 ccd() {
     cd $(chezmoi source-path)
 }
