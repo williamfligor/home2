9ea5d98754b7b53360f9dc0bcfc3cf3c42f3c255
William Fligor <williamtfligor@gmail.com>
2025-05-25 08:54:28 -0400

    feat: Add y shell function for yazi

--- .chezmoitemplates/functions.sh
+++ .chezmoitemplates/functions.sh
@@ -1,3 +1,12 @@
+# Yazi support
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
 # using ripgrep combined with preview
 # find-in-file - usage: fif <searchTerm>
 fif() {
