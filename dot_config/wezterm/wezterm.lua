c127fe63d2314a3859a88955dff91265b4e6e4e3
William Fligor <williamtfligor@gmail.com>
2025-12-05 16:31:50 -0500

    feat: Add scrollbar to wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -79,6 +79,20 @@ config.key_tables = {
 }
 
 ---------------------------------------------------------------------------------
+-------------------------------- Scrollbar --------------------------------------
+---------------------------------------------------------------------------------
+
+-- Hide the scrollbar when there is no scrollback or alternate screen is active
+wezterm.on("update-status", function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    local dimensions = pane:get_dimensions()
+
+    overrides.enable_scroll_bar = dimensions.scrollback_rows > dimensions.viewport_rows and not pane:is_alt_screen_active()
+
+    window:set_config_overrides(overrides)
+end)
+
+---------------------------------------------------------------------------------
 -------------------------------- TLS Clients ------------------------------------
 ---------------------------------------------------------------------------------
 
