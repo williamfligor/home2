d9bb470b7f5f7974215f341c06ea7aca47696fa4
William Fligor <williamtfligor@gmail.com>
2026-03-10 06:48:38 -0700

    fix: Add missing path to GOBIN

--- .chezmoitemplates/env.sh
+++ .chezmoitemplates/env.sh
@@ -18,6 +18,7 @@ export PATH="$HOME/.local/bin/nvim/bin/:$PATH"
 export PATH="$HOME/.local/bin/go/bin/:$PATH"
 export PATH="$HOME/.bun/bin/:$PATH"
 export PATH="$HOME/.cargo/bin/:$PATH"
+export PATH="$HOME/go/bin/:$PATH"
 
 if command -v nvim > /dev/null 2>&1; then
     export EDITOR="nvim"
